2f4b589c118f801c5ba10febaa386005faf7cf89
yoouyeon <jyeon.yoon59@gmail.com>
2023-09-24 00:54:41 +0900

    feat : add PrivateRoute

---
+++ src/PrivateRoute.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { ReactElement } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
+import { user } from 'utils/atom';
+import { getToken } from 'utils/axios';
+import { getUser } from 'api/users';
+
+interface PrivateRouteProps {
+  children?: ReactElement;
+  authentication: boolean;
+}
+
+const PrivateRoute = ({ children, authentication }: PrivateRouteProps) => {
+  const [userState, setUserState] = useRecoilState(user);
+
+  // 로그인을 하지 말아야 하는 경우
+  if (authentication === false) {
+    if (getToken()) return <Navigate to="/home" replace />;
+    else return <Outlet />;
+  }
+  if (getToken()) {
+    if (userState === null) {
+      getUser()
+        .then((res) => {
+          const { username, email, birth } = res.data;
+          setUserState({ username, email, birth });
+        })
+        .catch(() => {
+          alert('로그인이 필요한 페이지입니다.');
+          return <Navigate to="/login" replace />;
+        });
+    }
+    return <Outlet />;
+  }
+  return <Navigate to="/login" replace />;
+};
+
+export default PrivateRoute;
