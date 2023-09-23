e79fcf4eb76f6fe7ecf3d13db0fa23c01d08b922
yoouyeon <jyeon.yoon59@gmail.com>
2023-09-24 05:07:27 +0900

    chore : alert 주석

--- src/PrivateRoute.tsx
+++ src/PrivateRoute.tsx
@@ -27,7 +27,7 @@ const PrivateRoute = ({ children, authentication }: PrivateRouteProps) => {
           setUserState({ username, email, birth });
         })
         .catch(() => {
-          alert('로그인이 필요한 페이지입니다.');
+          // alert('로그인이 필요한 페이지입니다.');
           return <Navigate to="/login" replace />;
         });
     }
