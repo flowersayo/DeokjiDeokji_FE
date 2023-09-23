a9a588ddfd8aff4e1c8d44bac1f9bf9bedd5ca1b
yoouyeon <jyeon.yoon59@gmail.com>
2023-09-24 06:20:54 +0900

    feat : full data map

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-
+import React, { useEffect, useState } from 'react';
 import { Map, MapMarker } from 'react-kakao-maps-sdk';
 import { styled } from 'styled-components';
 import CreateRecordModal from 'component/modals/CreateRecordModal';
 import CreateRecordBtn from 'component/home/CreateRecordBtn';
 import { IPlace } from 'utils/interface';
 import { LocationInfo } from 'component/LocationInfo';
+import { GET } from 'utils/axios';
 
 declare global {
   interface Window {
@@ -17,45 +17,21 @@ const HomePage = () => {
   const [isCreateRecordModalOpen, setIsCreateRecordModalOpen] = useState(false);
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const [focused, setFocused] = useState<IPlace | null>(null);
+  const [locations, setLocations] = useState<IPlace[]>([]);
+
+  useEffect(() => {
+    GET('/api/v1/place')
+      .then((res) => {
+        setLocations(res.content);
+      })
+      .catch(() => {
+        alert('데이터를 불러오는데에 실패했습니다.');
+      });
+  }, []);
 
   const handleCreateBtnClick = () => {
     setIsCreateRecordModalOpen(true);
   };
-
-  const locations = [
-    {
-      id: 1,
-      name: '카카오',
-      address: '서울 마포구 마포대로 173-14 마포센텀슬로우',
-      latitude: 33.450705,
-      longitude: 126.570677,
-      category: 'restaurant',
-    },
-    {
-      id: 2,
-      name: '생태연못',
-      address: '서울 마포구 마포대로 173-14 마포센텀슬로우',
-      latitude: 33.450936,
-      longitude: 126.569477,
-      category: 'coffee',
-    },
-    {
-      id: 3,
-      name: '텃밭',
-      address: '서울 마포구 마포대로 173-14 마포센텀슬로우',
-      latitude: 33.450879,
-      longitude: 126.56994,
-      category: 'birthday',
-    },
-    {
-      id: 4,
-      name: '근린공원',
-      address: '서울 마포구 마포대로 173-14 마포센텀슬로우',
-      latitude: 33.451393,
-      longitude: 126.570738,
-      category: 'coffee',
-    },
-  ];
 
   return (
     <HomePageLayout>
