c367645537ef8e068e61e272cc5731a3edd2d42b
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2023-09-23 14:09:23 +0900

    feat : type에 따라 custom marker 표시

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -7,22 +7,58 @@ declare global {
   }
 }
 const HomePage = () => {
+  const locations = [
+    {
+      title: '카카오',
+      latlng: {
+        lat: 33.450705,
+        lng: 126.570677,
+      },
+      category: 'restaurant',
+    },
+    {
+      title: '생태연못',
+      latlng: {
+        lat: 33.450936,
+        lng: 126.569477,
+      },
+      category: 'coffee',
+    },
+    {
+      title: '텃밭',
+      latlng: {
+        lat: 33.450879,
+        lng: 126.56994,
+      },
+      category: 'birthday',
+    },
+    {
+      title: '근린공원',
+      latlng: {
+        lat: 33.451393,
+        lng: 126.570738,
+      },
+      category: 'coffee',
+    },
+  ];
+
   return (
     <>
       <Map
         center={{ lat: 33.5563, lng: 126.79581 }}
         style={{ width: '100vw', height: '100vh' }}
       >
-        <MapMarker
-          position={{ lat: 33.5563, lng: 126.79581 }}
-          image={{
-            src: '/assets/svg/restaurant.svg', // 마커이미지의 주소입니다
-            size: {
-              width: 24,
-              height: 29,
-            }, // 마커이미지의 크기입니다
-          }}
-        ></MapMarker>
+        {locations.map((loc, idx) => (
+          <MapMarker
+            key={`${loc.title}-${loc.latlng}`}
+            position={loc.latlng}
+            image={{
+              src: `/assets/svg/${loc.category}.svg`,
+              size: { width: 24, height: 35 },
+            }}
+            title={loc.title}
+          />
+        ))}
       </Map>
     </>
   );
