1dd843f9d488bd3ac400f7d01a8d279eb97352c5
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2023-09-23 11:50:24 +0900

    feat : kakaomap api 연결

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import styled from 'styled-components';
 
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
 const HomePage = () => {
-  return <div>home</div>;
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    let container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+    let options = {
+      //지도를 생성할 때 필요한 기본 옵션
+      center: new window.kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+      level: 3, //지도의 레벨(확대, 축소 정도)
+    };
+
+    let map = new window.kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+
+    // 클린업 함수 (컴포넌트가 언마운트될 때 맵 리소스 정리)
+    return () => {
+      map.destroy();
+    };
+  }, []);
+
+  return (
+    <div>
+      <MapContainer ref={mapRef} id="map" />
+    </div>
+  );
 };
+
+const MapContainer = styled.div`
+  width: 500vw;
+  height: 400vh;
+`;
 
 export default HomePage;
