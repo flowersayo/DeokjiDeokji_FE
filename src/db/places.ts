0cf0c2e802ee64d70d06a1259a65825b9de04f5e
flowersayo <flowersayo0811@ewhain.net>
2023-09-24 08:23:40 +0900

    fix : fix type enum

--- src/db/places.ts
+++ src/db/places.ts
@@ -5,7 +5,7 @@ export const places = [
     address: '서울특별시 중구 신당동 다산로 149',
     latitude: 37.55723,
     longitude: 127.011697,
-    type: 'restaurant',
+    type: 'Restaurant',
     img: 'https://lh5.googleusercontent.com/p/AF1QipO3y3BGlTnfVRVE4NBEql3F1b0UvHetXSysx-K2=w718-h538-p-k-no',
   },
   {
@@ -14,7 +14,7 @@ export const places = [
     address: '서울시 용산구 한강대로 10길 11-50',
     latitude: 37.524538,
     longitude: 126.964816,
-    type: 'birthday',
+    type: 'BirthCafe',
     img: 'https://pbs.twimg.com/media/F51tfX5aYAIUSqw?format=jpg&name=large',
   },
   {
@@ -23,7 +23,7 @@ export const places = [
     address: '서울 용산구 한강대로11길 4',
     latitude: 37.525121,
     longitude: 126.96339,
-    type: 'birthday',
+    type: 'BirthCafe',
     img: 'https://pbs.twimg.com/media/F6nCtXoboAAsvGQ?format=jpg&name=medium',
   },
   {
@@ -32,7 +32,7 @@ export const places = [
     address: '서울특별시 용산구 원효로1가 43-26',
     latitude: 37.539248,
     longitude: 126.96925,
-    type: 'restaurant',
+    type: 'Restaurant',
     img: 'https://lh5.googleusercontent.com/p/AF1QipNwHCyM8i2cMbkLHfscoQcAccWTpyRNErBBSPMB=w408-h544-k-no',
   },
   {
@@ -41,7 +41,7 @@ export const places = [
     address: '서울특별시 마포구 상수동 독막로15길 13-5',
     latitude: 37.548524,
     longitude: 126.921894,
-    type: 'coffee',
+    type: 'Cafe',
     img: 'https://lh5.googleusercontent.com/p/AF1QipOwigu_OjZLIlPDvYJBJgRZtXCvqN_btqdPCqTl=w444-h240-k-no',
   },
   {
@@ -50,7 +50,7 @@ export const places = [
     address: '서울특별시 용산구 한강대로15길 27',
     latitude: 37.526372,
     longitude: 126.962946,
-    type: 'restaurant',
+    type: 'Restaurant',
     img: 'https://lh5.googleusercontent.com/p/AF1QipM1WPYOsjDEhptJMnyRlhCB7UtE6l66VHem6ADo=w408-h510-k-no',
   },
 ];
