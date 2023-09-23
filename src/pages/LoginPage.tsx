366f41e823ae741804bed5093e3be7b48240ea7e
yoouyeon <jyeon.yoon59@gmail.com>
2023-09-23 16:49:15 +0900

    feat : kakao login button

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Title, Body2_1 } from 'src/styles/font';
 import { AUTH_URL } from 'src/utils/constants';
+import kakaoLoginButtonImg from 'src/assets/buttons/kakao_login_medium_wide.png';
 
 const LoginPage = () => {
   const clickLoginButton = () => {
@@ -12,7 +13,9 @@ const LoginPage = () => {
       <Title>덕지와 함께</Title>
       <Title>덕지순례 가실래요?</Title>
       <Body2_1>먼저 로그인이 필요해요 :)</Body2_1>
-      <button onClick={clickLoginButton}>카카오 로그인</button>
+      <button onClick={clickLoginButton}>
+        <img src={kakaoLoginButtonImg} alt="카카오 로그인 버튼" />
+      </button>
     </div>
   );
 };
