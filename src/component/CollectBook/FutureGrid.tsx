5784390a306cebb2ae2a202eb9c0c331baf43b61
yoouyeon <jyeon.yoon59@gmail.com>
2023-09-24 10:41:26 +0900

    test : FutureGrid

--- src/component/CollectBook/FutureGrid.tsx
+++ src/component/CollectBook/FutureGrid.tsx
@@ -4,18 +4,43 @@ import { FutureCollect, FutureCollectProps } from './FutureCollect';
 import { CollectBookGridWrapper } from './CollectComponent';
 
 export const FutureGrid = () => {
-  const [data, setData] = useState<FutureCollectProps[]>([]);
+  const [data, setData] = useState<FutureCollectProps[]>([
+    {
+      id: 1,
+      tag: 'BirthCafe',
+      name: '카페 세화',
+      artist: '세븐틴_민규',
+    },
+    {
+      id: 2,
+      tag: 'BirthCafe',
+      name: '카페 단',
+      artist: '뉴진스_민지',
+    },
+    {
+      id: 3,
+      tag: 'Restaurant',
+      name: '하이디라오',
+      artist: '세븐틴_준',
+    },
+    {
+      id: 4,
+      tag: 'Cafe',
+      name: '더카페 마룬에프',
+      artist: '블랙핑크_로제',
+    },
+  ]);
 
-  useEffect(() => {
-    GET('/api/v1/user/booking-storage')
-      .then((res) => {
-        setData(res);
-      })
-      .catch(() => {
-        alert('데이터를 불러오는 데 실패했습니다.');
-        return;
-      });
-  }, []);
+  // useEffect(() => {
+  //   GET('/api/v1/user/booking-storage')
+  //     .then((res) => {
+  //       setData(res);
+  //     })
+  //     .catch(() => {
+  //       alert('데이터를 불러오는 데 실패했습니다.');
+  //       return;
+  //     });
+  // }, []);
 
   if (!data) {
     return <div>데이터가 없습니다.</div>;
