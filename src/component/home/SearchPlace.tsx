537c0341a1dbc4558f5c56fa8d5c23623b8e9004
flowersayo <flowersayo0811@ewhain.net>
2023-09-29 16:03:00 +0900

    feat: place search (kakao map API)

--- src/component/home/SearchPlace.tsx
+++ src/component/home/SearchPlace.tsx
@@ -3,13 +3,16 @@ import { CustomFont } from 'styles/font';
 import { styled } from 'styled-components';
 import PlaceList from './PlaceList';
 import { IPlace } from 'utils/interface';
+import SearchIconSrc from 'assets/icons/search.svg';
 
+/* https://apis.map.kakao.com/web/sample/keywordList/ */
 const SearchPlace = ({ place, setPlace }: { place: IPlace; setPlace: any }) => {
   const [placeListVisible, setPlaceListVisible] = useState(false); // 장소 리스트
 
   const [inputValue, setInputValue] = useState<string>(place?.name);
 
-  const places: IPlace[] = [
+  const initialPlaces: IPlace[] = [
+    /*
     {
       id: 0,
       name: '공덕 프론트원',
@@ -50,20 +53,60 @@ const SearchPlace = ({ place, setPlace }: { place: IPlace; setPlace: any }) => {
       longitude: 101,
       address: '서울 마포구 와우산로 161 성경빌딩 2층 203호',
     },
+    */
   ];
+
+  const [results, setResults] = useState(initialPlaces);
+
+  // 장소 검색
+  const handleClickSearch = (keyword: string) => {
+    const ps = new kakao.maps.services.Places();
+
+    ps.keywordSearch(keyword, (data, status, _pagination) => {
+      console.log(data);
+      if (status === kakao.maps.services.Status.OK) {
+        console.log(data);
+
+        // 데이터 정제
+        const newResults: IPlace[] = [];
+
+        for (const item of data) {
+          const { address_name, id, place_name, place_url, x, y } = item;
+          const newItem = {
+            address: address_name,
+            type: place.type, // 기존에 선택된 카테고리 재활용
+            id: parseInt(id),
+            name: place_name,
+            latitude: parseFloat(x),
+            longitude: parseFloat(y),
+            img: place_url,
+          };
+          newResults.push(newItem);
+        }
+        setResults(newResults);
+      }
+    });
+  };
+
   return (
     <Box>
-      <Input
-        placeholder="덕지순례 갈 원하는 장소를 입력해주세요"
-        onFocus={() => setPlaceListVisible(true)}
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
+      <Row>
+        <Input
+          placeholder="덕지순례 갈 원하는 장소를 입력해주세요"
+          onFocus={() => setPlaceListVisible(true)}
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
 
-        // onBlur={() => setPlaceListVisible(false)}
-      />
+          // onBlur={() => setPlaceListVisible(false)}
+        />
+        <Img
+          src={SearchIconSrc}
+          onClick={() => handleClickSearch(inputValue)}
+        />
+      </Row>
       {placeListVisible && (
         <PlaceList
-          places={places}
+          places={results}
           setPlace={(place: IPlace) => {
             setPlace(place);
             setInputValue(place.name);
@@ -78,6 +121,16 @@ const Box = styled.div`
   display: flex;
   flex-direction: column;
   gap: 26px;
+  max-height: 350px;
+`;
+
+const Row = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 11px;
 `;
 const Input = styled.input`
   display: flex;
@@ -97,4 +150,11 @@ const Input = styled.input`
     outline: 1px solid black;
   }
 `;
+
+const Img = styled.img`
+  width: 24px;
+  height: 24px;
+  cursor: pointer;
+`;
+
 export default SearchPlace;
